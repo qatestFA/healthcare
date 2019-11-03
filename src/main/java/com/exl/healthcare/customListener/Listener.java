34d979219af909170a7247725f57d033365c22a8
qatestFA <qa.fahim.ahmad@gmail.com>
2019-11-03 23:42:28 +0530

    commited

--- src/main/java/com/exl/healthcare/customListener/Listener.java
+++ src/main/java/com/exl/healthcare/customListener/Listener.java
@@ -16,31 +16,43 @@ import org.testng.Reporter;
 import com.exl.healthcare.testBase.TestBase;
 
 public class Listener extends TestBase implements ITestListener{
-	
+
 	public void onFinish(ITestContext result) {
-		// TODO Auto-generated method stub
-		
+
+		Calendar calender = Calendar.getInstance();
+		SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+		String methodName = result.getName();
+		File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String reportdirectory = new File(System.getProperty("user.dir")).getAbsolutePath();
+		File destFile = new File((String) reportdirectory + "/failure_screenshot/" + methodName + "_" + formater.format(calender.getTime())+ ".png");
+		try {
+			FileHandler.copy(srcFile, destFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		Reporter.log("<a href='"+destFile.getAbsolutePath()+"'> <img src='"+destFile.getAbsolutePath()+"' height='100' width='100'> </a>");
+
 	}
 
 	public void onStart(ITestContext result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailure(ITestResult result) {
-		
+
 		Calendar calender = Calendar.getInstance();
 		SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-		
+
 		String methodName = result.getName();
-		
+
 		if(!result.isSuccess()) {
-			
+
 			File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 			String reportdirectory = new File(System.getProperty("user.dir")).getAbsolutePath();
 			File destFile = new File((String) reportdirectory + "/failure_screenshot/" + methodName + "_" + formater.format(calender.getTime())+ ".png");
@@ -51,38 +63,22 @@ public class Listener extends TestBase implements ITestListener{
 			}	
 			Reporter.log("<a href='"+destFile.getAbsolutePath()+"'> <img src='"+destFile.getAbsolutePath()+"' height='100' width='100'> </a>");
 		}
-		
+
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		Calendar calender = Calendar.getInstance();
-		SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-		
-		String methodName = result.getName();
-		
-		if(result.isSuccess()) {
-			
-			File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-			String reportdirectory = new File(System.getProperty("user.dir")).getAbsolutePath();
-			File destFile = new File((String) reportdirectory + "/failure_screenshot/" + methodName + "_" + formater.format(calender.getTime())+ ".png");
-			try {
-				FileHandler.copy(srcFile, destFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
-			Reporter.log("<a href='"+destFile.getAbsolutePath()+"'> <img src='"+destFile.getAbsolutePath()+"' height='100' width='100'> </a>");
-		}
-		
+
+
 	}
 
 }
